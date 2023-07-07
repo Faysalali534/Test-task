953c0a94ef8d95b35ce48bc4b5ea14aceac5f86e
faisal <faysalali534@gmail.com>
2023-07-07 16:13:24 +0500

    Implement logout functionality and session management
    - Implemented session clearing and logout functionality
    - Enhanced user session management and security
    
    This commit introduces the logout functionality, allowing users to log out of the application.

--- product_manager/settings.py
+++ product_manager/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 
 from .views import SignupView
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('auth/signup/', SignupView.as_view(), name='auth-signup'),
     path('auth/login/', TokenObtainPairView.as_view(), name='auth-login'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/logout', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
