4eaa594658339048b288bf761521c8a9cd2b37c2
faisal <faysalali534@gmail.com>
2023-07-07 15:41:13 +0500

    Add refresh token functionality for enhanced user sessions
    - Implemented refresh token generation during login
    - Added API endpoint for refreshing access tokens
    - Updated authentication settings to include refresh token support
    - Enhanced session management and user experience with refreshed tokens
    
    This commit introduces the refresh token functionality, allowing users to refresh their access tokens to extend their session duration. It includes the implementation of refresh token generation, the addition of an API endpoint for token refreshing, and updates to the authentication settings to support refresh tokens. With this enhancement, users can enjoy uninterrupted sessions and an improved user experience.

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import SignupView
 
 urlpatterns = [
     path('auth/signup/', SignupView.as_view(), name='auth-signup'),
     path('auth/login/', TokenObtainPairView.as_view(), name='auth-login'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
